dd83595d40ec4deff22fb9449d0f2021b8c73129
Desmond Williams <desmond@desfolio.com>
2020-05-13 17:16:18 -0400

    Version 1 done. Task completed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace WhatCentury
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.Write("Enter a four digit year: ");
+                var year = Convert.ToInt32(Console.ReadLine());
+                var century = year / 100;
+
+                if (year % 100 != 0)
+                {
+                    century += 1;
+                }
+
+                Console.WriteLine($"{year} is in Century {century}.");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please enter a valid year.");
+            }
+            
         }
     }
 }
