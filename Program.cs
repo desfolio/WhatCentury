ac52060995876db268c92e8064f3d11367cc657e
Desmond Williams <desmond@desfolio.com>
2020-05-13 17:02:55 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WhatCentury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+#Direction
+## Write a program to get the century from a year.
+Example:
+- Input year: 2020
+- Output: 21
